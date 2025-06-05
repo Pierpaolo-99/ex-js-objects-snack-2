e346bfb45b28148069662be41fb6fd10a51bda18
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-06-05 15:20:02 +0200

    Add initial implementation of hamburger object and cloning example

---
+++ Code_Question_2/main.js
@@ -0,0 +1,15 @@
+const hamburger = {
+    name: "Cheese Burger",
+    weight: 250,
+    ingredients: ["Cheese", "Meat", "Bread", "Tomato"]
+};
+
+const secondBurger = { ...hamburger };
+secondBurger.ingredients[0] = "Salad";
+
+console.log(hamburger.ingredients[0]); // Salad
+console.log(secondBurger.ingredients[0]); // Salad
+
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+
+// risposta: 3
