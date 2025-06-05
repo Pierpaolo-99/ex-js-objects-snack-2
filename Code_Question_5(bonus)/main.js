5ec49d63ec54707f6e7506b9692268ac6d8a80a1
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-06-05 15:42:42 +0200

    Add comments to clarify object creation and memory usage in hamburger example

--- Code_Question_5(bonus)/main.js
+++ Code_Question_5(bonus)/main.js
@@ -23,3 +23,7 @@ console.log(hamburger.maker.name); // Chef Hyur
 console.log(secondBurger.maker.name); // Chef Hyur
 console.log(hamburger.maker.restaurant.name); // Hyur's II
 console.log(secondBurger.maker.restaurant.name); // Hyur's II
+
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+
+// risposta: 5 oggetti
