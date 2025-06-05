560f0c86c7a5ca050d3e76469a56c9a827a72937
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-06-05 15:15:31 +0200

    first commit

---
+++ Code_Question_1/main.js
@@ -0,0 +1,11 @@
+const hamburger = { name: "Cheese Burger", weight: 250 };
+const secondBurger = hamburger;
+secondBurger.name = 'Double Cheese Burger';
+secondBurger.weight = 500;
+
+console.log(hamburger.name); // Double Cheese Burger
+console.log(secondBurger.name); // Double Cheese Burger
+
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+
+// risponsta: 1
